f816a4843c45e2a1b7c719c05aafc1cdd8e760f9
Panthomema <71283471+Panthomema@users.noreply.github.com>
2025-07-16 12:20:15 +0200

    Install clsx + test

--- src/components/Scoreboard/Scoreboard.tsx
+++ src/components/Scoreboard/Scoreboard.tsx
@@ -1,6 +1,9 @@
+import styles from '@/components/Scoreboard/Scoreboard.module.css';
+import clsx from 'clsx';
+
 function Scoreboard() {
   return (
-    <div>
+    <div className={clsx(styles.scoreboard, 'modal')}>
       <div>Score: 0</div>
       <div>Best Score: 0</div>
     </div>
