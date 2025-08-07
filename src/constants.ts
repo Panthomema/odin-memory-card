16030f5385b806ca8737e2ab9cb1c0d16a255a53
Panthomema <71283471+Panthomema@users.noreply.github.com>
2025-08-07 11:45:26 +0200

    Change pokemon list

--- src/constants.ts
+++ src/constants.ts
@@ -5,42 +5,55 @@ export const POKEMON_PER_ROUND = 6;
 export const GHOST_ID = 105;
 
 export const POKEMON_IDS = [
+  // Lavender Tower core
   92, // Gastly
   93, // Haunter
+  94, // Gengar
   104, // Cubone
   105, // Marowak
 
-  41, // Zubat
-  42, // Golbat
-  63, // Abra
-  66, // Machop
-  67, // Machoke
-  74, // Geodude
-  75, // Graveler
-  79, // Slowpoke
-  88, // Grimer
-  89, // Muk
+  // Creepy, eerie, or unusual
+  50, // Diglett
+  51, // Dugtrio
   96, // Drowzee
   97, // Hypno
-  109, // Koffing
+  122, // Mr. Mime
+  49, // Venomoth
   110, // Weezing
-  111, // Rhyhorn
-  118, // Goldeen
-  119, // Seaking
 
-  23, // Ekans
-  24, // Arbok
-  28, // Sandslash
-  30, // Nidorina
-  31, // Nidoran♂
-  32, // Nidorino
+  // Sludgy, ghostly, or mysterious
+  88, // Grimer
+  89, // Muk
+  109, // Koffing
+  108, // Lickitung
+  137, // Porygon
+
+  // Nearby routes/caves
+  41, // Zubat
+  42, // Golbat
+  66, // Machop
+  74, // Geodude
+  75, // Graveler
   56, // Mankey
   58, // Growlithe
+  81, // Magnemite
+  82, // Magneton
+
+  // Rare, strange or creepy-looking Pokémon
+  113, // Chansey
+  114, // Tangela
+  115, // Kangaskhan
+  127, // Pinsir
+  124, // Jynx
+  102, // Exeggcute
+  103, // Exeggutor
+
+  // Water/route Pokémon
   72, // Tentacool
-  73, // Tentacruel
-  101, // Electrode
-  108, // Lickitung
+  90, // Shellder
+  98, // Krabby
   130, // Gyarados
-  137, // Porygon
-  122, // Mr. Mime
+
+  // Final oddball
+  85, // Dodrio
 ];
