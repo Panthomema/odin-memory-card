c348317d67e615ab2ae39757a98b23a470ac370d
Panthomema <71283471+Panthomema@users.noreply.github.com>
2025-08-15 18:10:26 +0200

    Configure Vite to use svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import path from 'path';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });
